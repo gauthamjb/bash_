4087e5616fd9d05a4a5cd54c22bdac9932943e7d
Gautham Kannan JB <74786785+gauthamjb@users.noreply.github.com>
2023-08-15 13:56:01 +0530

    Update hard1.sh

--- hard1.sh
+++ hard1.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 read -p "enter work done: " tgm
 read -p "enter time worked: " tmw
-telegram-cli -k tg-server.pub -WE -e "msg Bi0s_Pentest_Freshers_2020 Namah Shivaya                                                                               Name Gautham                                                           Work done:$tgm                                                    Time worked:$tmw "
+telegram-cli -k tg-server.pub -WE -e "msg Bi0s Namah Shivaya                                                                               Name Gautham                                                           Work done:$tgm                                                    Time worked:$tmw "
